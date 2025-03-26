ce691d756c65e4d9a90f6591daa41fe1fd174626
Jesse Hoekema <109305882+JesseHoekema@users.noreply.github.com>
2025-03-26 17:56:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def create_image(text):
     return img_io
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7856)))
